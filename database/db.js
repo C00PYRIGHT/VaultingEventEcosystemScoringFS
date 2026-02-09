8a6bba39a8b1c0d5296437d7f634de364d605d30
C00PYRIGHT <bence.daniel99@gmail.com>
2026-02-09 17:07:42 +0100

    Logging debug

--- database/db.js
+++ database/db.js
@@ -2,18 +2,19 @@ import mongoose from 'mongoose';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import dotenv from 'dotenv';
-import {logger, logError, logInfo} from "../logger.js";
+import {logger, logError, logInfo, logDb, logWarn} from "../logger.js";
 import { MONGODB_URI } from '../app.js';
 async function connectDB() {
     try {
         // Adatbázis kapcsolódás
         await mongoose.connect(MONGODB_URI);
-        logInfo('Successfully connected to MongoDB');
+        logDb('CONNECT','Successfully connected to MongoDB', '');
         // Kapcsolat lezárása kilépéskor
         process.on('SIGINT', async () => {
             try {
                 await mongoose.disconnect();
-                logInfo('Connection to MongoDB closed.');
+                logDb('DISCONNECT','Connection to MongoDB closed.', '');
+                
                 process.exit(0);
             } catch (err) {
                 logError('DB_DISCONNECT', 'Failed to disconnect from MongoDB', err.toString());
